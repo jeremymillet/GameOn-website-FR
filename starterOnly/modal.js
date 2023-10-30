b958ebe8ccdd157c3a65c07117ded72ac51c29ac
jeremy millet <jeremy.millet37@gmail.com>
2023-10-30 14:56:06 +0100

    submit message

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -13,6 +13,8 @@ const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const modalCloseBtn = document.querySelectorAll(".close");
 const modalBody = document.querySelector(".modal-body");
+const modalForm = document.querySelector(".modal-form")
+const btnSubmit = document.querySelector(".btn-submit")
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -26,3 +28,15 @@ function launchModal() {
 function closeModal() {
   modalbg.style.display = "none";
 }
+
+modalForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const success = document.createElement('div');
+  const  successMessage = document.createElement('p');
+  success.classList.add('success');
+  successMessage.classList.add('success-message');
+  successMessage.innerHTML = 'Merci pour <br/>votre inscription';
+  modalBody.removeChild(modalForm);
+  modalBody.appendChild(success);
+  success.appendChild(successMessage);
+});
