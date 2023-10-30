7d0f75a4f4b28fbabf904fdc305788254fa62891
jeremy millet <jeremy.millet37@gmail.com>
2023-10-30 14:54:31 +0100

    close modal

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,13 +11,18 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const modalCloseBtn = document.querySelectorAll(".close");
+const modalBody = document.querySelector(".modal-body");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+modalCloseBtn.forEach((btn) => btn.addEventListener("click", closeModal));
 
 // launch modal form
 function launchModal() {
-  modalbg.style.display = "block";
+  modalbg.style.display = "flex";
 }
 
-
+function closeModal() {
+  modalbg.style.display = "none";
+}
